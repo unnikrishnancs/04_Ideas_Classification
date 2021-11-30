fbf4281b42dd271c8aac7a2c5f075bea4b870c41
unnikrishnan <unnikrishnan.cs.13@gmail.com>
2021-11-30 20:16:46 +0530

    train model...label encode y..getting error when training..presence of nan value

--- 02_TrainModel.py
+++ 02_TrainModel.py
@@ -2,15 +2,26 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.preprocessing  import OneHotEncoder, LabelEncoder,LabelBinarizer
-#from sklearn.model_selection import train_test_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
+#to display all rows and cols
+#pd.set_option('display.max_columns',10)
+#pd.set_option('display.max_rows',None)
+
+#=========================
 #import data from csv file
+#=========================
+
 data=pd.read_csv("bangalore_rentdtls_updt.csv",na_values=["-"])
 print(data.head())
 
 #column data types and non-null values
 data.info()
+
+#summary of numeric columans
+print(data.describe())
 
 # ???????? TRY TO FIND SOLN....handle "L" (for Lakhs in "4.5 L") in MinPrice and MaxPrice columsn ....FOR NOW, DELETE IT MANUALLY
 #
@@ -36,8 +47,6 @@ for col in data.select_dtypes("object"):
 data.info()
 
 
-#summary of numeric columans
-print(data.describe())
 
 #handle outliers
 
@@ -53,8 +62,6 @@ print()
 #========================================================================
 
 # NOTE : *** this conversion to be put into NEW module / class and call it ***
-
-
 
 ohe=OneHotEncoder()
 
@@ -77,6 +84,7 @@ print()
 print("Input features concatenated")
 data_features_final=pd.concat([data_newdf,data[["MinPrice","MaxPrice","AvgRent"]]],axis=1)
 print(data_features_final.head())
+print()
 
 #=================================================
 #convert categorical to numeric (LABELS / CLASSES)
@@ -86,30 +94,67 @@ le=LabelEncoder()
 le.fit(data["HouseType"])
 print(le.classes_)
 data_out=le.transform(data["HouseType"])
-print(data_out[:10])
+print(data_out[:6])
+print()
 
 
-
-#.fit(data[["Locality","HouseType"]])
-
+# TO DO....
 #try with and without LabelBinarizer
-
 #feature scaling (run model WITH / WITHOUT feature scaling)
 
-
 #join all the columsn into one (i:e input features + class))
+print("Final dataset before training")
+data_final=pd.concat([data_features_final,pd.Series(data_out)],axis=1)
+print(data_final.head())
+print(data.info())
+print()
 
 
+
+'''
+#====================
+#Define X and y
+#====================
+
+X=data_final.iloc[:,:-1]
+#print(X.head())
+
+y=data_final.iloc[:,-1]
+#print(y[:5])
+print()
+
+#print(data_final.describe())
+#pd.set_option('display.max_columns',10)
+#pd.set_option('display.max_rows',None)
+#print(data_final[0].value_counts())
+data_final.to_csv("exported_data_final.csv")
+
+#==================================
 #split data into Train and Test set
+#==================================
+
+train_x,test_x,train_y,test_y=train_test_split(X,y,test_size=0.2,random_state=2)
 
 
+#===========
 #train model
+#===========
+
+print(type(train_x),type(train_y))
+model=LogisticRegression()
+model.fit(train_x,train_y)
 
 
+
+#=======
 #predict
+#=======
+predict_y=model.predict(test_x)
 
-
+#=======
 #metrixs
+#=======
 
-
-
+#print confusion matrix
+print(confusion_matrix(test_y,predict_y))
+'''
